249cf6dbfe96bd6162af58c6ffa81801d43c0e81
Umayrrrr <umairbacha00@gmail.com>
2026-01-30 11:03:19 +0500

    home changes minor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
-        <div>
-            <h2>Welcome to the Home Page</h2>
+        <div className="flex h-full justify-center items-center">
             
+            <h1 className="font-bold font-serif shadow-xl">Pakhair raghle</h1>
         </div>
     );
 }
