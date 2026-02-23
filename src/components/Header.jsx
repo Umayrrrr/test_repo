25a90449e104f741c1fefc0ad85f62e6f0a56a99
Umayrrrr <umairbacha00@gmail.com>
2026-02-23 15:50:53 +0500

    abc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { redirect } from "react-router";
 import { useNavigate } from "react-router";
 import { supabase } from "../supabase-client";
 export default function Header({ onToggle }) {
+<<<<<<< HEAD
   const navigate = useNavigate();
   const logout = () => {
     supabase.auth.signOut().then(() => {
@@ -29,3 +30,22 @@ export default function Header({ onToggle }) {
     </header>
   );
 }
+=======
+    return (
+
+        <header className="bg-gray-800 text-white py-3 pb-0 px-6 shadow-md flex ">
+            <h1 className="text-3xl font-bold">My App</h1>
+            <div className="flex ">
+                <button 
+                    
+                    onClick={onToggle}
+                    className="py-3 px-3 hover:bg-gray-700 cursor-pointer "
+                    title="Toggle sidebar"
+                >
+                    <MoreVertical  />
+                </button>
+            </div>
+        </header>
+    );
+}
+>>>>>>> f07a3d7 (adding contact form)
